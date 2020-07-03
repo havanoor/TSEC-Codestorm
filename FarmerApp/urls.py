f91e62bf625f8ff15dd31d3627c4264be26980e9
Harsh Vartak <vartak.harsh@gmail.com>
2020-07-03 11:58:44 +0530

    Removed testing sides

--- FarmerApp/urls.py
+++ FarmerApp/urls.py
@@ -10,13 +10,7 @@ urlpatterns=[
     path('order/<str:pref>',views.order,name = "order"),
     path('farmershop/',views.farmersell,name = "FS"),
 
-
-
-
     path('signup',views.register, name='signup'),
-    path('signup/farmer', views.register_farmer, name='farmer_add'),
-    path('signup/buyer',views.register_buyer, name='buyer_add'),
-    path('login/', views.login_view, name='login'),
     path('farmer',views.farmerHome, name='farmer_home'),
     path('buyer',views.buyerHome, name='buyer_home'),
 ]
